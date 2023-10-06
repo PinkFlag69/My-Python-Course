641440e7d2f833258d12f963efb7a4c07403e316
Dario Gerald <antgera7@gmail.com>
2023-10-06 20:56:48 +1100

    Pushed

---
+++ Chapter 5/If Elif and Else Statements.py
@@ -0,0 +1,12 @@
+'''
+Control flow:
+In our programs, we often want certain conditions to be met before certain code runs.
+For example, if my dog was hungry then -> I'd feed my dog.
+
+To control this flow of logic we use some keywords:
+* If
+* Elif
+* Else
+
+Control flow 
+'''
